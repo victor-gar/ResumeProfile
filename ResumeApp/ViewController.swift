91e01bd91aa9d29660c11c1b134719ac879cb0d8
Victor Garitskyu <victorgaritskyu@MacBook-Pro-Victor-2.local>
2023-08-06 10:28:11 +0500

    commit

---
+++ ResumeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResumeApp
+//
+//  Created by Victor Garitskyu on 06.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
